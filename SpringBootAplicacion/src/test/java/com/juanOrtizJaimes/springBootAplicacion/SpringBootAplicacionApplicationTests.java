dbe62a4d32cf601c455e8bee76dd6f5c63ff1f15
JuanPabloOrtizJaimes <juanpablooj@ufps.edu.co>
2022-05-23 14:37:15 -0500

    Commit inicial con sls. Close #1

---
+++ SpringBootAplicacion/src/test/java/com/juanOrtizJaimes/springBootAplicacion/SpringBootAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanOrtizJaimes.springBootAplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
